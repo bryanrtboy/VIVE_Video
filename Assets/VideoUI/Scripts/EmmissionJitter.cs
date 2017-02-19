19b631b63198928a5a73edccd7ccea31ea473bfd
bryanrtboy <bryan@bryanleister.com>
2017-02-18 18:13:53 -0700

    Added Assets

---
+++ Assets/VideoUI/Scripts/EmmissionJitter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmmissionJitter : MonoBehaviour
+{
+
+	public AnimationCurve m_curve;
+
+
+	public float m_emissionScale = 1.0F;
+	public float m_speed = 1.0F;
+
+	Material m_mat;
+	Color baseColor = Color.white;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Renderer m_renderer = this.GetComponent<Renderer> () as Renderer;
+
+		if (m_renderer == null)
+			Destroy (this);
+
+		m_mat = m_renderer.material;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		float emission = m_emissionScale * Mathf.PerlinNoise (Time.time * m_speed, 0.0F);
+
+		Color finalColor = baseColor * Mathf.LinearToGammaSpace (emission);
+ 
+		m_mat.SetColor ("_EmissionColor", finalColor);
+
+		
+	}
+
+
+
+}
---
+++ Assets/VideoUI/Scripts/FadeVideoTexture.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeVideoTexture : MonoBehaviour
+{
+
+	public float m_fadeDuration = 2f;
+
+	private bool m_isFadedOut = false;
+
+	public void Fade ()
+	{
+		if (!m_isFadedOut) {
+			StartCoroutine (FadeOut3D (this.transform, 0f, true, m_fadeDuration));
+		} else {
+			this.transform.gameObject.SetActive (true);
+			StartCoroutine (FadeOut3D (this.transform, 1f, false, m_fadeDuration));
+		}
+		m_isFadedOut = !m_isFadedOut;
+
+	}
+
+	IEnumerator FadeOut3D (Transform _transform, float targetAlpha, bool isVanish, float duration)
+	{
+
+		Renderer sr = _transform.GetComponent<Renderer> ();
+		float diffAlpha = (targetAlpha - sr.material.color.a);
+ 
+		float counter = 0;
+		while (counter < duration) {
+			float alphaAmount = sr.material.color.a + (Time.deltaTime * diffAlpha) / duration;
+			sr.material.color = new Color (sr.material.color.r, sr.material.color.g, sr.material.color.b, alphaAmount);
+ 
+			counter += Time.deltaTime;
+			yield return null;
+		}
+		sr.material.color = new Color (sr.material.color.r, sr.material.color.g, sr.material.color.b, targetAlpha);
+		if (isVanish) {
+			sr.transform.gameObject.SetActive (false);
+		}
+	}
+}
